5ae83938452f5f3774bceb9120f6054a567a8272
Aman Kaushik <amankaushik481@gmail.com>
2022-07-24 18:30:54 +0530

    done

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <nav className="navbar bg-light mb-4 p-0">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          <div className="d-flex">
+            <div>MERN App</div>
+          </div>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 const colors = require("colors");
 
 const { graphqlHTTP } = require("express-graphql");
@@ -11,6 +12,7 @@ const app = express();
 
 connectDB();
 
+app.use(cors());
 app.use(
   "/graphql",
   graphqlHTTP({
